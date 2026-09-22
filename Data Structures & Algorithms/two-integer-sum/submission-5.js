dc2500a0824b5ade2896be9072d125a67c332446
Fahad Ali Khan <fk99970@gmail.com>
2026-09-22 09:05:07 +0500

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let indices = [];
+        const map = {};
+        const NUMS_LENGTH = nums.length;
+        for(let i = 0; i < NUMS_LENGTH; i++) {
+            const element = nums[i];
+            const difference = target - element;
+            if(Object.hasOwn(map, difference)) {
+                const secIdx = map[difference];
+                indices = [i, secIdx];
+                break;
+            } else {
+                map[element] = i;
+            }
+        }
+        return indices;
+    }
+}
